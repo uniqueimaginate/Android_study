36492833a1781f5a81a653700877590e07e045b2
Peter <uniqueimaginate@gmail.com>
2019-02-13 23:24:23 +0900

    Chapter1

---
+++ Chapter1/MyBasicWidget/app/src/main/java/org/techtown/basicwidget/MainActivity.java
@@ -0,0 +1,18 @@
+package org.techtown.basicwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioButton radioButton = (RadioButton)findViewById(R.id.radioButton);
+        boolean checked = radioButton.isChecked();
+
+    }
+}
